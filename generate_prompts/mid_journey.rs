32e040c6944fd4b0a3e0d8253b1d290cd8e0cc8a
Fankai Liu <liufankai137@outlook.com>
2023-04-15 17:43:05 +0800

    ✨try to support wasm

--- generate_prompts/mid_journey.rs
+++ generate_prompts/mid_journey.rs
@@ -2,13 +2,6 @@ use rand::{Rng, seq::SliceRandom};
 
 use crate::config::load_cofig::CFG;
 
-// #[derive(Debug, serde::Deserialize)]
-// pub struct Configs {
-//     pub must_appear:Vec<String>,
-//     pub may_appear:HashMap<String,u32>,
-//     pub pick_one:Vec<HashMap<String,u32>>,
-// }
-
 pub struct MidJourney;
 
 impl MidJourney {
